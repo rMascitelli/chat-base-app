cd8af97491b1af8b16e61765746845e358ca836d
Robert Mascitelli <robertmascitelli@gmail.com>
2023-06-01 18:27:55 -0400

    adding helpers.go, JSON functions

--- back-end/cmd/api/handlers.go
+++ back-end/cmd/api/handlers.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
 type CustomRequest struct {
 	Title string `json:"title"`
-}
-
-func (app *Config) decodeJSONRequest(r *http.Request) error {
-	log.Println("Trying to decode JSON")
-
-	// Declare a new Person struct.
-	var c CustomRequest
-
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&c)
-	if err != nil {
-		fmt.Println("FAILURE while decoding")
-		return err
-	}
-
-	// Do something with the Person struct...
-	fmt.Printf("CustomRequest: %+v", c)
-	return nil
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -41,14 +14,9 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Error:   false,
 		Message: "Hit the broker",
 	}
+	var c CustomRequest
 
-	err := app.decodeJSONRequest(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	} else {
-		out, _ := json.MarshalIndent(payload, "", "\t")
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusAccepted)
-		w.Write(out)
-	}
+	app.readJSON(w, r, &c)
+	fmt.Printf("CustomRequest: %+v", c)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
