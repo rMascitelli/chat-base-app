704d11d9a5e89afe68b15f77f3ea6e0c062b48d8
Robert Mascitelli <robertmascitelli@gmail.com>
2023-06-01 20:43:59 -0400

    Sending back credentials

--- back-end/cmd/api/handlers.go
+++ back-end/cmd/api/handlers.go
@@ -6,15 +6,15 @@ import (
 )
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
-		Error:   false,
-		Message: "Hit the broker",
-	}
-
 	// Just expecting Auth requests for now
 	var auth AuthPayload
 
 	app.readJSON(w, r, &auth)
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+		Data:    auth,
+	}
 	fmt.Printf("Auth credentials: %+v", auth)
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
